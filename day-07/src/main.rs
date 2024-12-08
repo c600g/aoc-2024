75f2a3c73ee90f0b4eacfbb57d098c10cdec9db5
Alan Krause <alan.krause@gmail.com>
2024-12-07 19:36:56 -0800

    Bug fix

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
         s = s.trim().to_string();
         if count > 0 && s.len() > 0 {
             let colon = s.find(":").unwrap();
-            let sum: usize = (&s[..colon - 1].trim()).to_string().parse().expect("Can not parse.");
+            let sum: usize = (&s[..colon].trim()).to_string().parse().expect("Can not parse.");
             let args = (&s[colon + 1..].trim()).to_string();
             let args: Vec<usize> = args.split_whitespace().filter_map(|s| s.parse().ok()).collect();
             println!("sum:{} args: {:?}", sum, args);
