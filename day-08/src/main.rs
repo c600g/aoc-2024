27aea24f4d7b2d5e93abfb16c85a001cb21c2fd9
Alan Krause <alan.krause@gmail.com>
2024-12-08 21:07:21 -0800

    Started work on day 8

---
+++ day-08/src/main.rs
@@ -0,0 +1,77 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+struct Map {
+    chars: Vec<char>,
+    cols: usize,
+    rows: usize,
+}
+
+impl Map {
+    fn new() -> Self {
+        Self {
+            chars: Vec::new(),
+            cols: 0,
+            rows: 0,
+        }
+    }
+
+    fn from_file(filename: &str) -> Self {
+        let file = File::open("test-input.txt").unwrap();
+        let mut buf = BufReader::new(file);
+        //let mut s = String::new();
+        let mut chars = Vec::new();
+        let mut cols = 0;
+        let mut rows = 0;
+
+        loop {
+            let mut s = String::new();
+            let count = buf.read_line(&mut s).unwrap();
+            s = s.trim().to_string();
+            if count > 0 && s.len() > 0 {
+                cols = s.len();
+                let mut string_chars: Vec<char> = s.chars().collect();
+                chars.append(&mut string_chars);
+                rows += 1;
+            } else {
+                break;
+            }
+        }
+        Self { chars, cols, rows }
+    }
+
+    fn from_cols_rows(cols: usize, rows: usize) -> Self {
+        let mut chars: Vec<char> = Vec::new();
+        for i in 0..cols * rows {
+            chars.push('.');
+        }
+        Self { chars, cols, rows }
+    }
+
+    fn print(&self) {
+        for (i, c) in self.chars.iter().enumerate() {
+            if i % self.cols == 0 {
+                print!("\n");
+            }
+            print!("{}", c);
+        }
+        println!("");
+    }
+
+    fn col_row_from_index(&self, index: usize) -> (usize, usize) {
+        (index % self.cols, index / self.cols)
+    }
+
+    fn index_from_col_row(&self, col: usize, row: usize) -> usize {
+        row * self.cols + col
+    }
+}
+
+fn main() {
+    let map = Map::from_file("test-input.txt");
+    println!("Map is {} cols by {} rows.", map.cols, map.rows);
+    map.print();
+
+    let mut antinodes = Map::from_cols_rows(map.cols, map.rows);
+    antinodes.print();
+}
